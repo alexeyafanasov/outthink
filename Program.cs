0c800d49cd68a283ecc810edb1e87bf70dd9119e
alexeyafanasov <afanasov@mail.com>
2019-01-21 18:40:34 +0300

    calc

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // System.Console.WriteLine("MutenSudak");
+            Calc.Add(-10, 20);
+            System.Console.ReadKey();
+        }
+    }
+    class Calc
+    {
+        public static void Add(int x, int y)
+        {
+            int z = x + y;
+            System.Console.WriteLine($"{x} + {y} = {z}");
+        }
+    }
+}
